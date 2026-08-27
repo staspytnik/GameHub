4b14bfb16951b6fd739fed38e9932a848b2e038e
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-27 11:37:39 +0300

    feat: improve closing modal logic

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -5,22 +5,30 @@
  * Open the modal with the given content.
  * @param {string} html
  */
-const modalDataAtribute = 'data-modal'
+const modalDataAttribute = 'data-modal'
+const modalCloseDataAttr = '[data-modal-close]'
+
 const modalOverlay = document.createElement('div')
 modalOverlay.classList.add('modal-overlay')
 
+let modalClose;
+
 export function openModal(html) {
   // TODO: implement modal rendering and open state.
-    if (!modalOverlay.hasAttribute(modalDataAtribute)) {
+    if (!modalOverlay.hasAttribute(modalDataAttribute)) {
         const modalWindow = document.createElement('div')
 
         modalWindow.classList.add('modal-window')
-        modalOverlay.setAttribute(modalDataAtribute, '')
+        modalOverlay.setAttribute(modalDataAttribute, '')
 
         modalWindow.innerHTML = html
 
         modalOverlay.appendChild(modalWindow)
         document.body.appendChild(modalOverlay)
+
+        modalClose = modalWindow.querySelector(modalCloseDataAttr)
+
+        modalClose.addEventListener('click', closeModal)
     }
 }
 
@@ -29,7 +37,9 @@ export function openModal(html) {
  */
 export function closeModal() {
   // TODO: implement modal close/cleanup.
+    modalClose.removeEventListener('click', closeModal)
+    modalClose = null;
     modalOverlay.innerHTML = ''
-    modalOverlay.removeAttribute(modalDataAtribute)
+    modalOverlay.removeAttribute(modalDataAttribute)
     modalOverlay.remove()
 }
