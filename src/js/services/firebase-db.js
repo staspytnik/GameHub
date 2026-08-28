bd6ccc465499a6ed4307fb0c594be3aee028531d
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-28 18:34:13 +0300

    feat: create and initialized firebase database

---
+++ src/js/services/firebase-db.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5vTJQA-xYzT8sJsKLMw7L69vE4_0-r1w",
+    authDomain: "gamehub---library.firebaseapp.com",
+    projectId: "gamehub---library",
+    storageBucket: "gamehub---library.firebasestorage.app",
+    messagingSenderId: "606270874971",
+    appId: "1:606270874971:web:90be49d2062f958015667e",
+    measurementId: "G-P85ET4Z8W7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
