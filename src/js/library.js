04d47433f3c735b74fca7cd9c7dc198704cf66e9
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-08-31 14:41:56 +0300

    feat: rendering cards on load page

--- src/js/library.js
+++ src/js/library.js
@@ -4,14 +4,52 @@ import {getGamesData} from "./services/firebase-db.js";
 
 const libraryAddGameButton = document.querySelector('.library__button')
 
+const libraryGamesList = document.querySelector('.library-games-list');
+
 const init = () => {
     libraryAddGameButton.addEventListener('click', () => {
         openModal(modalTemplate)
     })
 }
 
+const renderGames = (data) => {
+    data.map(game => {
+        const cardElement = document.createElement('article')
+        cardElement.classList.add('game-card')
+        cardElement.innerHTML = `
+            <div class="game-card__cover">
+                <img
+                  src="#"
+                  alt="game cover"
+                />
+              </div>
+            
+              <div class="game-card__content">
+                <div class="game-card__top">
+                  <h3 class="game-card__title">${game.name}</h3>
+            
+                  <span class="game-card__status status--want-to-play">
+                    ${game.status}
+                  </span>
+                </div>
+            
+                <div class="game-card__meta">
+                  <span class="game-card__genre">${game.genre}</span>
+                  <span class="game-card__separator">•</span>
+                  <span class="game-card__year">${game.year}</span>
+                </div>
+            </div>
+        `
+        libraryGamesList.appendChild(cardElement)
+    })
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    getGamesData().then(data => console.log(data));
+    getGamesData().then(data => {
+        console.log(Object.values(data))
+        renderGames(Object.values(data))
+    });
 })
 
 init()
