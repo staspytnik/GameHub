8a75e4ba462ec6dfa4177784e20277d7ec0d210d
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-09-02 19:18:39 +0300

    feat: somr modal refactors

--- src/js/services/formatData.js
+++ src/js/services/formatData.js
@@ -1,5 +1,19 @@
 export const formatData = (form) => {
     const formData = new FormData(form);
 
-    return Object.fromEntries(formData);
-}
+    return Object.fromEntries(
+        [...formData.entries()].map(([key, value]) => {
+            if (value instanceof File) {
+                return [key, value];
+            }
+
+            if (value === 'true') return [key, true];
+            if (value === 'false') return [key, false];
+            if (value !== '' && !Number.isNaN(Number(value))) {
+                return [key, Number(value)];
+            }
+
+            return [key, value];
+        })
+    );
+};
