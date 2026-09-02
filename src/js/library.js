da2ecb564a27df7b12c6678eafe64c519553487e
Nazar Galicia <nazargaliciaworkemail@gmail.com>
2026-09-02 17:27:16 +0300

    refactor: move game card rendering to Handlebars

--- src/js/library.js
+++ src/js/library.js
@@ -1,12 +1,17 @@
 import {openModal} from "./components/modal.js";
 import modalTemplate from './templates/library-modal.hbs?raw'
 import {getGamesData} from "./services/firebase-db.js";
-import placehplderImage from '../../public/images/card-image-placeholder.png'
+import placeholderImage from '../../public/images/card-image-placeholder.png'
+import gameCardTemplate from './templates/library-card.hbs?raw';
+import Handlebars from "handlebars";
 
 const libraryAddGameButton = document.querySelector('.library__button')
 
 const libraryGamesList = document.querySelector('.library-games-list');
 const emptyStateElement = document.querySelector('.library-empty');
+
+const template = Handlebars.compile(gameCardTemplate);
+Handlebars.registerHelper('eq', (a, b) => a === b);
 
 const init = () => {
     libraryAddGameButton.addEventListener('click', () => {
@@ -15,52 +20,11 @@ const init = () => {
 }
 
 const renderGames = (data) => {
-    data ? Object.values(data).map(game => {
-        let statusCard;
-
-        switch (game.status) {
-            case 'want-to-play': {
-                statusCard = 'status--want-to-play';
-                break;
-            }
-            case 'completed': {
-                statusCard = 'status--completed';
-                break;
-            }
-            case 'playing': {
-                statusCard = 'status--playing';
-                break;
-            }
-        }
-
-        const cardElement = document.createElement('article')
-        cardElement.classList.add('game-card', statusCard)
-        cardElement.innerHTML = `
-            <div class="game-card__cover">
-                <img
-                  src="${placehplderImage}"
-                  alt="game cover"
-                />
-              </div>
-            
-              <div class="game-card__content">
-                <div class="game-card__top">
-                  <h3 class="game-card__title">${game.name}</h3>
-            
-                  <span class="game-card__status status--want-to-play ${statusCard}">
-                    ${game.status}
-                  </span>
-                </div>
-            
-                <div class="game-card__meta">
-                  <span class="game-card__genre">${game.genre}</span>
-                  <span class="game-card__separator">•</span>
-                  <span class="game-card__year">${game.year}</span>
-                </div>
-            </div>
-        `
-        libraryGamesList.appendChild(cardElement)
-    }) : emptyStateElement.classList.add('library-empty--active')
+    data ? libraryGamesList.innerHTML = template({
+        games: Object.values(data),
+        placeholderImage: placeholderImage,
+    })
+    : emptyStateElement.classList.add('library-empty--active')
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
