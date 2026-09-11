aaf6bee072ae4bd5d0ed85746276b9c5b312afc9
staspytnik <ggmavpa8@gmail.com>
2026-09-11 18:10:15 +0300

    Add favorite functionality to game cards and update active state styling

--- src/js/components/allGames.js
+++ src/js/components/allGames.js
@@ -141,3 +141,24 @@ pagesWrap.addEventListener("click", async (e) => {
   pagination.page = page;
   await loadGames();
 });
+
+refs.gamesList.addEventListener("click", async (event) => {
+  const likeBtn = event.target.closest(".game-card__likeBtn");
+  if (!likeBtn) return;
+
+  const card = likeBtn.closest(".game-card");
+  const gameId = card.dataset.id;
+
+  const savedFavorites = JSON.parse(localStorage.getItem("library")) || [];
+
+  if (!savedFavorites.includes(gameId)) {
+    const gameCard = await fetchGameById(gameId);
+    savedFavorites.push(gameCard);
+    localStorage.setItem("library", JSON.stringify(savedFavorites));
+    likeBtn.classList.add("is-active");
+  } else {
+    const updatedFavorites = savedFavorites.filter((id) => id !== gameId);
+    localStorage.setItem("library", JSON.stringify(updatedFavorites));
+    likeBtn.classList.remove("is-active");
+  }
+});
